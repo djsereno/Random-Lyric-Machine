6cf367129859424b86536af74d9418ac43ae373f
djsereno <djsereno91@gmail.com>
2022-06-08 00:53:36 -0400

    inital commit

---
+++ random-lyric-machine/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id='quote-box'>
+      <p id='text'>This is a quote</p>
+      <p id='author'>- Author</p>
+      <button id='new-quote'>New Lyric</button>
+      <a id='tweet-quote' href=''>Tweet It</a>
+    </div>
+  );
+}
+
+export default App;
