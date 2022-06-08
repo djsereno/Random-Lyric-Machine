c26c86a4447ff63e211cb1e16a0ef6b654f75bdb
djsereno <djsereno91@gmail.com>
2022-06-08 02:06:53 -0400

    random lyric functionality implemented

--- random-lyric-machine/src/App.js
+++ random-lyric-machine/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 
+const lyricData = require('./random-lyrics.json');
+
 function App() {
+  const lyric = RandomLyric();
   return (
     <div id='quote-box'>
-      <p id='text'>This is a quote</p>
-      <p id='author'>- Author</p>
+      <p id='text'>"{lyric.lyric}"</p>
+      <p id='author'>- {lyric.artist}</p>
       <button id='new-quote'>New Lyric</button>
       <a id='tweet-quote' href=''>Tweet It</a>
     </div>
   );
+}
+
+function RandomLyric() {
+  return lyricData[Math.floor(Math.random()*lyricData.length)];
 }
 
 export default App;
