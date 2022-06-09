684f084e5ed47db4371c856677f4e886138b7431
djsereno <djsereno91@gmail.com>
2022-06-09 00:56:55 -0400

    new lyric button functionality

--- random-lyric-machine/src/App.js
+++ random-lyric-machine/src/App.js
@@ -1,20 +1,48 @@
+import React from 'react';
 import './App.css';
 
 const lyricData = require('./random-lyrics.json');
 
-function App() {
-  const lyric = RandomLyric();
-  return (
-    <div id='quote-box'>
-      <p id='text'>"{lyric.lyric}"</p>
-      <p id='author'>- {lyric.artist}</p>
-      <button id='new-quote'>New Lyric</button>
-      <a id='tweet-quote' href=''>Tweet It</a>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lyric: null,
+      artist: null
+    }
+  }
+
+  updateLyric() {
+    const randomLyric = getRandomLyric();
+    this.setState({
+      lyric: randomLyric.lyric,
+      artist: randomLyric.artist
+    });
+  }
+  
+  render() {
+    if (!this.state.lyric) {
+      this.updateLyric();
+    }
+    
+    return (
+      <div id='quote-box'>
+        <p id='text'>"{this.state.lyric}"</p>
+        <p id='author'>- {this.state.artist}</p>
+        <button
+          id='new-quote'
+          onClick={() => this.updateLyric()}
+        >New Lyric</button>
+        <a
+          id='tweet-quote'
+          href='twitter.com/intent/tweet'
+        >Tweet It</a>
+      </div>
+    );
+  }
 }
 
-function RandomLyric() {
+function getRandomLyric() {
   return lyricData[Math.floor(Math.random()*lyricData.length)];
 }
 
